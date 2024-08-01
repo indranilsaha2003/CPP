6fc48f9d72d80717585b94e4b98e495324819e03
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-08-01 20:25:26 +0530

    Update div.cpp

--- Calculator/div.cpp
+++ Calculator/div.cpp
@@ -6,7 +6,7 @@ void calculator::divDigits(int n1,int n2){
     num1=n1;
     num2=n2;
     if (n2 != 0) {
-        cout << "The result of dividing " << n1 << " by " << n2 << " is: " << num1 / num2 <<endl;
+        cout << "The result of dividing " << n1 << " by " << n2 << " is: " << static_cast<float>(num1)/num2 <<endl;
     }
 
 }
