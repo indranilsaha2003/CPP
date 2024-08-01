34736ac54b064f4e6ae4c7ab8e82096ad71bda26
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-08-01 19:34:45 +0530

    Add files via upload

---
+++ add_constructor/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class addNum{
+    private:
+        int a;
+        int b;
+    public:
+    addNum(int num1, int num2){
+        a = num1;
+        b = num2;
+        cout << "The sum is : " << a+b << endl;
+    }
+};
+
+int main(){
+    int n1, n2;
+    cout << "Enter the first no. : " << endl;
+    cin >> n1;
+    cout << "Enter the second no. : " << endl;
+    cin >> n2;
+    addNum(n1,n2);
+    return 0;
+}
