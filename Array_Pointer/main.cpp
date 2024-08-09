ea6f5c1c0a317daa5027484ac1ca29a95d4723ed
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-08-09 19:37:00 +0530

    Update main.cpp

--- Array_Pointer/main.cpp
+++ Array_Pointer/main.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
        int n, i;
        cout << "Enter The Size Of the Array : ";
        cin >> n;
        int arr[n];
        cout << "Enter The Elements : " << endl;
-       for (i = 0; i < n; i++)
-       {
+       for (i = 0; i < n; i++){
               cin >> arr[i];
        }
        cout << endl;
        int *ptr = arr;
        cout << "Array using pointer : " << endl;
-       for (int i = 0; i < n; i++)
-       {
+       for (int i = 0; i < n; i++){
               cout << *ptr << " ";
               ptr++;
        }
        cout << endl;
        cout << endl;
        cout << "Array using array name : " << endl;
-       for (int i = 0; i < n; i++)
-       {
-              cout << *arr + i << " ";
+       for (int i = 0; i < n; i++){
+              cout << *(arr + i) << " ";
        }
        cout << endl;
        cout << endl;
        int *arrptr[n];
-       for (int i = 0; i < n; i++)
-       {
+       for (int i = 0; i < n; i++){
               arrptr[i] = &arr[i];
        }
        cout << "Array using array of pointers : " << endl;
-       for (int i = 0; i < n; i++)
-       {
+       for (int i = 0; i < n; i++){
               cout << *arrptr[i] << " ";
        }
+       cout << endl;
        cout << endl;
        return 0;
 }
